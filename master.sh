94dc78feea8750ed9dcbdba47a0863460c7419fe
Dmitrii Tcimbal <dtcimbal@gmail.com>
2020-11-22 17:59:18 +1100

    Adds a description for netfilter for the flag net.bridge.bridge-nf-call-iptables

--- master.sh
+++ master.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-set -xe
+set -x
 
 NODE=master
 
 mkdir -p .kube
+mkdir -p .iptables
 
-multipass launch ubuntu --name ${NODE} --cpus 2 --mem 2G --disk 8G
+multipass launch ubuntu --name ${NODE} --cpus 2 --mem 2G --disk 8G 2> /dev/null
 multipass mount "$(pwd)/docker" ${NODE}:/home/ubuntu/docker 2> /dev/null 
 multipass mount "$(pwd)/kube" ${NODE}:/home/ubuntu/kube 2> /dev/null 
 multipass mount "$(pwd)/node" ${NODE}:/home/ubuntu/node 2> /dev/null
+
+set -e
 
 echo "Setting up prerequisites"
 multipass exec ${NODE} -- bash -c 'sudo apt update && sudo apt install -y ansible'
@@ -18,17 +21,25 @@ multipass exec ${NODE} -- bash -c 'ansible-playbook docker/docker.yml'
 multipass exec ${NODE} -- bash -c 'ansible-playbook kube/kube.yml'
 multipass exec ${NODE} -- bash -c 'ansible-playbook node/node.yml'
 
+multipass exec ${NODE} -- bash -c 'sudo iptables -L -n -t nat' > .iptables/master.initial
+
 echo "Setting up Master"
 multipass exec ${NODE} -- bash -c 'kubeadm config images pull'
 multipass exec ${NODE} -- bash -c 'sudo kubeadm init --pod-network-cidr=192.178.0.0/16' 2> /dev/null
 multipass exec ${NODE} -- bash -c 'sudo cat /etc/kubernetes/admin.conf' > .kube/config
 
+# multipass exec ${NODE} -- bash -c 'sudo iptables -L -n -t nat' > .iptables/master.complete
+
 echo "Installing CNI Network Plugin"
 KUBECONFIG=.kube/config kubectl create -f calico/tigera-operator.yml
 KUBECONFIG=.kube/config kubectl create -f calico/custom-resources.yml
 
-sleep 10
+sleep 60
 KUBECONFIG=.kube/config kubectl rollout status daemonset calico-node -n calico-system
+
+multipass exec ${NODE} -- bash -c 'sudo iptables -L -n -t nat' > .iptables/master.complete
+
+# diff .iptables/master.initial .iptables/master.complete > .iptables/master.diff
 
 mv ~/.kube/config ~/.kube/config.back
 cp .kube/config ~/.kube/config
